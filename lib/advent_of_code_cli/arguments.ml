d3ea8b26c94a1f452b8878617a9596d3f9b1f284
Yannick Koller <yannick.koller@econ.uzh.ch>
2025-11-13 14:53:57 +0000

    chore: improve and align documentation

--- lib/advent_of_code_cli/arguments.ml
+++ lib/advent_of_code_cli/arguments.ml
@@ -1,22 +1,22 @@
 open Cmdliner
 
 let session_cookie_arg =
-  let doc = "The session cookie for accessing the Advent of Code Website and API." in
+  let doc = "The $(docv) for accessing the Advent of Code API / Website." in
   Arg.(
     required
     & opt (some string) None
     & info
-        ~env:(Cmd.Env.info "ADVENT_OF_CODE_SESSION_COOKIE")
+        ~env:(Cmd.Env.info "ADVENT_OF_CODE_SESSION_COOKIE" ~doc)
         ~docv:"COOKIE"
-        ~doc
+        ~doc:
+          (doc ^ " Will be read from the environment variable '$(env)' if not specified.")
         [ "s"; "session-cookie" ]
   )
 ;;
 
 let year =
   let doc =
-    "The year of the event to download inputs from. Defaults to the last active event \
-     year."
+    "Target the event that was held in $(docv). Defaults to the last active event."
   in
   let converter =
     let parser =
@@ -35,14 +35,14 @@ let year =
   Arg.(
     value
     & opt converter (Advent_of_code.Event.get_latest ())
-    & info ~doc ~docv:"YYYY" [ "y"; "year" ]
+    & info ~doc ~docv:"YEAR" [ "y"; "year" ]
   )
 ;;
 
 let day =
   let doc =
-    "The day of the puzzle (1 - 25). Defaults to the currently active puzzle day if \
-     there is an ongoing Advent of Code event."
+    "Target the puzzle that was released on the n-th $(docv) of an event. Defaults to \
+     the currently active puzzle day if there is an ongoing Advent of Code event."
   in
   let current_puzzle_day_opt =
     let open Commons.Option.Infix in
@@ -51,32 +51,30 @@ let day =
   Arg.(
     required
     & opt (some int) current_puzzle_day_opt
-    & info ~doc ~docv:"day" [ "d"; "day" ]
+    & info ~doc ~docv:"DAY" [ "d"; "day" ]
   )
 ;;
 
 let output =
-  let doc =
-    "The file to write the puzzle input to. Use '-' to write to STDOUT. Defaults to \
-     STDOUT."
-  in
+  let doc = "The $(docv) to write to. Use '-' to write to STDOUT. Defaults to STDOUT." in
   let converter =
-    Arg.conv'
-      ( (function
-          | "" | "-" -> Ok None
-          | s ->
-            let prompt_overwrite filename =
-              Printf.printf "⚠️  File '%s' already exists. Overwrite? (y/N): " filename;
-              String.lowercase_ascii (read_line ()) = "y"
-            in
-            if Sys.file_exists s && not (prompt_overwrite s)
-            then Error (Printf.sprintf "⚠️  Cancelled: File '%s' was not overwritten" s)
-            else Ok (Some s)
-          )
-      , fun f -> function
-          | None -> Format.fprintf f "STDOUT"
-          | Some s -> Format.fprintf f "%s" s
-      )
+    let parser = function
+      | "" | "-" -> Ok None
+      | s ->
+        let prompt_overwrite filename =
+          Printf.printf "⚠️  File '%s' already exists. Overwrite? (y/N): " filename;
+          String.lowercase_ascii (read_line ()) = "y"
+        in
+        if Sys.file_exists s && not (prompt_overwrite s)
+        then Error (Printf.sprintf "⚠️  Cancelled: File '%s' was not overwritten" s)
+        else Ok (Some s)
+    in
+    let printer =
+      fun f -> function
+        | None -> Format.fprintf f "STDOUT"
+        | Some s -> Format.fprintf f "%s" s
+    in
+    Arg.conv' (parser, printer)
   in
   Arg.(value & opt converter None & info ~doc ~docv:"FILE" [ "o"; "output" ])
 ;;
